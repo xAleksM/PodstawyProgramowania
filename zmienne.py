7cbb92d45eda1f30ae72d8dd1432e3915368567f
uczen1C <uczen1C@serwer.pl>
2025-10-07 14:06:18 +0200

    Lekcja na temat zmiennych

---
+++ zmienne.py
@@ -0,0 +1,32 @@
+#Co tutaj się dzieje:
+#1) rezerwujemy kawałek pamięci
+#2) umieszczamy w tej pamięci liczbę 45
+#3) nadajemy temu kawałkowi pamięci liczba całkowita
+
+liczba_calkowita = 45
+print(type(liczba_calkowita)) #sprawdzamy i wypisujemy typ zmiennej
+
+liczba_z_przecinkiem = 2.65
+print(type(liczba_z_przecinkiem))
+
+'''
+To są pozostałe
+typy zmiennych
+również są świetne
+'''
+
+napis = 'informatyka'
+print(type(napis))
+
+wartosc_logiczna = True
+print(type(wartosc_logiczna))
+
+lista = [5, 'x', [5, 7], [[4, 6], 9],True, 'wsad']
+print(type(lista))
+
+zbior = {5, 5, 5, 6, 1, 9}
+print(zbior)
+print(type(zbior))
+
+slownik = {'dekadentyzn': 'prąd filozoficzny głoszący, że nid nie ma sensu', 5: 'pięć'}
+print(type(slownik))
