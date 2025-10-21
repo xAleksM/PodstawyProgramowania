d03cc9b8f589f6e7ab05a6cac45d43a35490cbda
uczen1C <uczen1C@serwer.pl>
2025-10-21 14:35:09 +0200

    instrukcje warunkowe

---
+++ instrukcje_warunkowe.py
@@ -0,0 +1,43 @@
+#Instrukcja warunkowa if
+
+#Przykład 1
+'''liczba = int(input('Podaj liczbę od 1 do 3'))
+
+if liczba == 1:
+    print('jeden')
+if liczba == 2:
+    print('dwa')
+if liczba == 3:
+    print('trzy')
+
+#Przykład 2
+liczba = int(input('Podaj liczbę od 1 do 3'))
+
+if liczba == 1:
+    print('jeden')
+elif liczba == 2:
+    print('dwa')
+elif liczba == 3:
+    print('trzy')
+
+#Przykład 3
+liczba = int(input('Podaj liczbę całkowitą dodatnią'))
+
+if liczba % 2 == 0:
+    print('parzysta')
+if liczba % 3 == 0:
+    print('Podzielna przez 3')
+if liczba > 0:
+    print('Dodatnia')'''
+
+#Przykład 4
+liczba = int(input('Podaj liczbę od 1 do 3'))
+
+if liczba == 1:
+    print('jeden')
+elif liczba == 2:
+    print('dwa')
+elif liczba == 3:
+    print('trzy')
+else:
+    print('Czytać nie umiesz?')
