732ac3137e3ef7bcff29e821b0cedfce8ccde6f2
Aleksander <aleksander.miszka@outlook.com>
2025-10-26 12:51:26 +0100

    aktualizacja zadania 13

--- instrukcja_warunkowa_zadan.py
+++ instrukcja_warunkowa_zadan.py
@@ -1,3 +1,4 @@
+#zadanie 13
 '''Klasa ma być podzielona na dwie grupy, w zależności od wyniku testu kompetencji
 językowych z wybranego języka obcego lub oceny na świadectwie ukończenia szkoły
 podstawowej. Jeśli dany uczeń uzyskał z testu wynik powyżej 90% punktów lub jeśli
@@ -8,8 +9,15 @@ program sprawdzający, do jakiej grupy zakwalifikuje się dany uczeń. Liczbę p
 uzyskanych z testu i ocenę ze świadectwa wprowadzaj z klawiatury, a komunikat:
 „grupa zaawansowana” lub „grupa podstawowa” wyświetlaj na ekranie.'''
 
+#zmienne sprawdzające wyniki ucznia
+
 ocena = int(input('Podaj ocenę z wybranego języka obcego: '))
 test = int(input('Podaj wynik z testu kompetencji językowych wybranego języka obcego: '))
+
+#skrypt sprawdza do jakiej grupy dostanie się uczeń
+#jeśli sprawdzający zrobi błąd to program poda wynik
+#na podstawie drugiej wartości a jeżeli będą 2 błędy
+#to poprosi o spóbowanie ponownie
 
 if (ocena >= 5 and ocena <= 6) or (test >= 90 and test <=100):
     print('Grupa zaawansowana')
