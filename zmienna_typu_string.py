b15b4c123e7777cfe05340b6c4b11d168ed3d915
uczen1C <uczen1C@serwer.pl>
2025-10-30 12:35:44 +0100

    zmienna typu string cz.2

--- zmienna_typu_string.py
+++ zmienna_typu_string.py
@@ -1,3 +1,5 @@
+from os.path import split
+
 napis = 'informatyka'
 
 #Fagment tekstu
@@ -21,3 +23,49 @@ if 'a' in napis:
     print('należy')
 else:
     print('nie należy')
+
+#III. Łączenie napisów (konkatencja)
+napis2 = napis + 'jestnajlepsza'
+print(napis2)
+
+#IV. Funkcje zmiennych typu string
+
+#1) poszukiwanie danego fragmentu w tekście
+napis3 = 'matematyka'
+index_gdzie_jest = napis3.find('tem')
+print(index_gdzie_jest)
+
+napis4 = 'alabalalalabala'
+index_gdzie_jest2 = napis4.find('bala')
+index_gdzie_jest3 = napis4.find('bala', index_gdzie_jest2 + 1)
+index_gdzie_jest4 = napis4.find('xyz', index_gdzie_jest2 + 1)
+print(index_gdzie_jest2)
+print(index_gdzie_jest3)
+print(index_gdzie_jest4)
+
+if napis4.find('xyz') != -1:
+    print('xyz jest w napisie')
+else:
+    print('xyz nie jest w napisie')
+
+#2.)Podział tekstu na fragmenty
+piec_liczb = input('Podaj pięć liczb. Oddziel je przecinkiem')
+piec_liczb_po_podziale = piec_liczb.split(',')
+print(piec_liczb_po_podziale)
+trzecia_liczba = int(piec_liczb_po_podziale[2])
+print(trzecia_liczba)
+print(trzecia_liczba + 33)
+
+#3.) Łącznie napisów
+lista_napisow = ['Windows', 'jest', 'tworzony', 'dla', 'kasy']
+cale_zdanie = '@'.join(lista_napisow)
+print(cale_zdanie)
+
+lista_napisow2 = ['abc', 'xyz', 'bbc', 'tvn']
+cale_zdanie2 = '\n'.join(lista_napisow2)
+print(cale_zdanie2)
+
+#4.) Zliczanie danego znaku w tekście
+napis5 = 'prawdopodobieństwo'
+ile_razy_o = napis5.count('o')
+print(ile_razy_o)
